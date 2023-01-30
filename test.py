dcba67fd74fe547c2fb3d0e17bd0ceb9bcc3fbdc
Hamid <hordimad21@gmail.com>
2023-01-30 17:15:48 +0330

    initial commit

---
+++ test.py
@@ -0,0 +1,53 @@
+import threading
+from TradingView import TradingView
+symbols = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "BCHUSDT",
+    "XRPUSDT",
+    "EOSUSDT",
+    "LTCUSDT",
+    "TRXUSDT",
+    "ETCUSDT",
+    "LINKUSDT",
+    "XLMUSDT",
+    "ADAUSDT",
+    "XMRUSDT",
+    "DASHUSDT",
+    "ZECUSDT",
+    "XTZUSDT",
+    "BNBUSDT",
+    "ATOMUSDT",
+    "ONTUSDT",
+    "IOTAUSDT",
+    "BATUSDT",
+    "VETUSDT",
+    "NEOUSDT",
+    "QTUMUSDT",
+    "IOSTUSDT",
+    "THETAUSDT",
+    "ALGOUSDT",
+    "ZILUSDT",
+    "KNCUSDT",
+    "ZRXUSDT",
+    "COMPUSDT",
+    "OMGUSDT",
+    "DOGEUSDT",
+    "SXPUSDT",
+    "KAVAUSDT",
+    "BANDUSDT",
+    "WAVESUSDT",
+    "MKRUSDT",
+    "DOTUSDT",
+    "YFIUSDT",
+]
+
+for coin in symbols:
+    TV = TradingView()
+
+# def printit():
+#   threading.Timer(5, printit).start()
+#   # data = 
+#   # print(data)
+
+# printit()
--- websock.py
+++ websock.py
@@ -10,28 +10,7 @@ import datetime
 import pytz
 
 def on_message(ws, message):
-    a = ""
-    a = a + message + "\n"
-    str = re.search('"s":\[(.+?)\}\]', a)
-
-    if str is not None:
-        coin = re.search(r'(?<=\bname":")(\w+)', a)
-        out = str.group(1)
-        x = out.split(',{\"')
-        for xi in x:
-            xi = re.split('\[|:|,|\]', xi)
-            # ts = datetime.datetime.fromtimestamp(float(xi[4]), tz=pytz.UTC).strftime("%Y/%m/%d, %H:%M:%S")
-            results = {
-                'currency': coin,
-                'time': xi[4],
-                'open': xi[5],
-                'high': xi[6],
-                'low': xi[7],
-                'close': xi[8],
-                'volume': xi[9]
-            }
-        print("\n ++++++++ message ++++++++ \n {} \n ++++++++ message ++++++++ \n".format(results))
-
+    print(message)
 
 def on_error(ws, error):
     print("Error--------------> {}".format(error))
@@ -42,22 +21,24 @@ def on_close(ws, close_status_code, close_msg):
 def on_open(ws):
     print("Opened connection\n")
 
-class TradingView:
-    def __init__(self):
-        self.headers = json.dumps({'Origin': 'https://data.tradingview.com'})
-        # websocket.enableTrace(True)
-        self.ws = websocket.WebSocketApp("wss://data.tradingview.com/socket.io/websocket",
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://data.tradingview.com/socket.io/websocket",
                               on_open=on_open,
                               on_message=on_message,
                               on_error=on_error,
-                              on_close=on_close, header=self.headers)
-        self.ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
-        self.do(symbol='binance:BTCUSDT', since=1)
-        rel.signal(2, rel.abort)  # Keyboard Interrupt
-        rel.dispatch()
-    
-    def do(self, symbol, since: int):
-        print('just doing things')
+                              on_close=on_close)
+
+    ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
+    rel.signal(2, rel.abort)  # Keyboard Interrupt
+    rel.dispatch()
+
+class TV_Crawler:
+    def __init__(self, ws, symbol, since):
+        self.headers = json.dumps({'Origin': 'https://data.tradingview.com'})
+        websocket.enableTrace(True)
+        self.ws = ws
+
         session = self.generateSession()
         chart_session = self.generateChartSession()
         self.sendMessage(self.ws, "set_auth_token", ["unauthorized_user_token"])
@@ -77,6 +58,16 @@ class TradingView:
                          [chart_session, "symbol_1",
                           "=" + message])
         self.sendMessage(self.ws, "create_series", [chart_session, "s1", "s1", "symbol_1", "1", since])
+                
+    def do(self):
+        while True:
+            try:
+                result = self.ws.recv()
+                if result:
+                    return result
+            except Exception as e:
+                print(e)
+                break
 
     def generateSession(self):
         string_length = 12
@@ -107,3 +98,34 @@ class TradingView:
 
     def sendMessage(self, ws, func, args):
         ws.send(self.createMessage(func, args))
+
+    def translate(self, content, symbol):
+        a = ""
+        a = a + content + "\n"
+        str = re.search('"s":\[(.+?)\}\]', a)
+        if str is not None:
+            out = str.group(1)
+            x = out.split(',{\"')
+            for xi in x:
+                xi = re.split('\[|:|,|\]', xi)
+                # ts = datetime.datetime.fromtimestamp(float(xi[4]), tz=pytz.UTC).strftime("%Y/%m/%d, %H:%M:%S")
+                result = {
+                    'currency': symbol,
+                    'time': xi[4],
+                    'open': xi[5],
+                    'high': xi[6],
+                    'low': xi[7],
+                    'close': xi[8],
+                    'volume': xi[9]
+                }
+            return result
+
+
+# if __name__ == "__main__":
+#     crawler = TV_Crawler('binance:BTCUSDT', 2)
+#     crawler.do()
+
+
+    # ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
+    # rel.signal(2, rel.abort)  # Keyboard Interrupt
+    # rel.dispatch()
